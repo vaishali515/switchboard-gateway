4dc4fb2ce65609e72bee37214510d06099753e60
atulsingh <atulkumarsingh952@gmail.com>
2026-01-19 14:01:09 +0530

    deployment

--- src/main/java/com/SwitchBoard/Gateway/GatewayApplication.java
+++ src/main/java/com/SwitchBoard/Gateway/GatewayApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
